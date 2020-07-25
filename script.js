f2e840d2e1af8072acc895743af87e1aa113cfc3
Nomenjanahary Clopedia <clopedia.nom@onja.org>
2020-07-25 15:54:44 +0300

    Clean the index.html to make it readable and understandable.

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
 console.log('Cool');
 
+// Several variable that are for grabbing all the elements from the html file that might be used to build this project.
 const form = document.querySelector('form');
-// const addIngredients = document.querySelector('#ingredients');
-// const addSteps = document.querySelector('#steps');
 const addIngredients = document.querySelector('.add-ingredients-button');
 const addSteps = document.querySelector('.add-step-button');
 const submitButton = document.querySelector('.submitbttn');
 const ingredientsButton = document.querySelector('.ingredients-button-container');
 const stepsButton = document.querySelector('.step-button-container');
 
+
+// A function for the add ingredients button, which contains the html element for the new ingredient to be created.
 const handleClick = (event) => {
     event.preventDefault();
     const inputHTML = `
     <div>
-        <input type="text"><br>
+        <input type="text" id="ingredients" name="ingredients"><br>
     <div>
     `;
     event.target = inputHTML;
@@ -22,11 +23,13 @@ const handleClick = (event) => {
 
 }
 
+
+// A function for the add steps button, which contains the html element for the new step to be created.
 const handleStepButton = (event) => {
     event.preventDefault();
     const stepHTML = `
     <div>
-        <input type="text"><br>
+        <input type="text" id="steps" name="steps"><br>
     <div>
     `;
     event.target = stepHTML;
@@ -36,12 +39,9 @@ const handleStepButton = (event) => {
 }
 
 
-const handleSubmit = (event) => {
-    event.preventDefault;
-    console.log(form.value);
-};
 
 
+// All three event listners for the two add buttons and one submit button.
 addIngredients.addEventListener('click', handleClick);
 addSteps.addEventListener('click', handleStepButton);
 submitButton.addEventListener('submit', handleSubmit);
